3c4402d1867d7f27f43091198c2ee2efee4cbdc9
vineet-53 <vineetsinghnegi53@gmail.com>
2024-08-03 14:12:57 +0530

    remove useless nvim config, and added hyprland window rules
    removed nvim configs -> nvim-tree etc
    added new colorscheme to nvim

--- nvim/.config/nvim/lua/coder/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/coder/plugins/colorscheme.lua
@@ -1,60 +1,12 @@
-function ColorMyPencils(color)
-  color = color or "rose-pine-moon"
-  vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
-end
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = false,
-        bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = true,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    config = function()
-      require("tokyonight").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        transparent = true, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
-          keywords = { italic = false },
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark", -- style for floating windows
-        },
-      })
-      ColorMyPencils("gruvbox")
-    end,
-  },
+	{
+		"sainnhe/sonokai",
+		priority = 1000,
+		config = function()
+			vim.g.sonokai_transparent_background = "1"
+			vim.g.sonokai_enable_italic = "1"
+			vim.g.sonokai_style = "andromeda"
+			vim.cmd.colorscheme("sonokai")
+		end,
+	},
 }
--- nvim/.config/nvim/lua/coder/plugins/oil.lua
+++ nvim/.config/nvim/lua/coder/plugins/oil.lua
@@ -4,8 +4,7 @@ return {
     local oil = require("oil")
     vim.keymap.set("n", "<leader>e", oil.toggle_float, { desc = "open explorer" })
     vim.keymap.set("n", "t", oil.select, { desc = "select the file" })
-    -- vim.keymap.set("n", "<C-p>", oil.preview, { desc = "preview the file" })
-    vim.keymap.set("n", "<C-w><C-h>", oil.toggle_hidden, { desc = "toggle hidden files" })
+    -- vim.keymap.set("n", "<C-w><C-h>", oil.toggle_hidden, { desc = "toggle hidden files" })
     oil.setup()
   end,
 }
