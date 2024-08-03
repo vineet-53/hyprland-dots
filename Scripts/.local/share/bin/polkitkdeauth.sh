5f1b23801269f9c9f5ffa64298fd77a49344bab3
vineet-53 <vineetsinghnegi53@gmail.com>
2024-08-03 16:15:09 +0530

    changes to scripts,dunst
    mainly changed dunst config
    removed default polkit file

--- Scripts/.local/share/bin/polkitkdeauth.sh
+++ Scripts/.local/share/bin/polkitkdeauth.sh
@@ -1,10 +1,2 @@
-#!/usr/bin/env sh
-
-# Use different directory on NixOS
-if [ -d /run/current-system/sw/libexec ]; then
-    libDir=/run/current-system/sw/libexec
-else
-    libDir=/usr/lib
-fi
-
-${libDir}/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#!/bin/sh
+hyprctl dispatch exec /usr/lib/polkit-kde-authentication-agent-1
